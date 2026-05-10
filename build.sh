d0fd0d83197c0208accaa02a64b539622c618177
Neel Gundale <neelgundale@gmail.com>
2026-05-10 16:47:28 +0530

    fix: build.sh executable + seed_data command

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py seed_data --equipment-count 300
+python manage.py seed_data 
